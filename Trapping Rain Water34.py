7aac28dd41a7815ff40d89c3fed9c290d89619a3
khushigupta-kg <khushigupta0987654321@gmail.com>
2025-10-29 16:32:09 +0530

    Implement trap function to calculate trapped rainwater

---
+++ Trapping Rain Water34.py
@@ -0,0 +1,19 @@
+def trap(height):
+    l, r = 0, len(height) - 1
+    left_max = right_max = 0
+    water = 0
+
+    while l < r:
+        if height[l] < height[r]:
+            if height[l] >= left_max:
+                left_max = height[l]
+            else:
+                water += left_max - height[l]
+            l += 1
+        else:
+            if height[r] >= right_max:
+                right_max = height[r]
+            else:
+                water += right_max - height[r]
+            r -= 1
+    return water
